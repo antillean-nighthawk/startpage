dbc08e28b2a92331b133e1ffa4a04fe4d113b303
antillean-nighthawk <95203358+antillean-nighthawk@users.noreply.github.com>
2025-01-26 22:22:45 -0800

    more links and engine toggle

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function displayDate() {
     document.getElementById('current-date').textContent = dateString;
 }
 
+// search engine
+function toggleEngine() {
+    var gSearch = document.getElementById("google-search");
+    var dSearch = document.getElementById("ddg-search");
+    var logo = document.getElementById("engine-logo").src;
+
+    if (logo.indexOf("duck.svg") != -1) {
+        document.getElementById("engine-logo").src = "imgs/google.svg";
+        gSearch.style.display = "block";
+        dSearch.style.display = "none";
+    } else {
+        document.getElementById("engine-logo").src = "imgs/duck.svg";
+        dSearch.style.display = "block";
+        gSearch.style.display = "none";
+    }
+
+    document.getElementsByClassName("input-box").reset();
+}
